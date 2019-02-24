03bbd0b12d57c125d0a234e3f155ab1dd4aa6f66
James Ferguson <jferguson@berkeley.edu>
2019-02-23 17:56:06 -0800

    run_server formatting

--- run_server.sh
+++ run_server.sh
@@ -3,5 +3,7 @@
 
 export DISPLAY=`/opt/TurboVNC/bin/vncserver -securitytypes tlsnone,x509none,none 2>&1 | perl -0777 -ne '/started on display .*?(:[0-9]*)/ && print $1'`
 echo "Started server on display $DISPLAY. To stop it, call stop_server.sh $DISPLAY"
-echo "If you did not source this script (ie called via \". run_script.sh\"), remember to update the DISPLAY variable: export DISPLAY=$DISPLAY"
+echo
+echo "If you did not source this script (ie called via \". run_script.sh\"),
+remember to update the DISPLAY variable: export DISPLAY=$DISPLAY"
 echo
